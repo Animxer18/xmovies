3c3bdfafffb10008c712b6111ab91ff6e3181cb3
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2023-10-06 20:36:07 +0800

    Update Footer.tsx

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className="sm:flex sm:items-center sm:justify-between">
         <a href="#" className="flex items-center mb-4 sm:mb-0">
             
-            <span className={`self-center text-2xl text-gray-200 font-black whitespace-nowrap `}>OTTO MOVIES</span>
+            <span className={`self-center text-2xl text-gray-200 font-black whitespace-nowrap `}>XERMOVIES</span>
         </a>
         <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-400 sm:mb-0">
             <li>
@@ -28,7 +28,7 @@ function Footer() {
     <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" className="hover:underline">OTTOMOVIES</a>. All Rights Reserved.
 
     </span>
-    <small className='text-gray-600 '>Made by Otto Programmer</small>
+    <small className='text-gray-600 '>Made By: XERMOVIES</small>
     </div>
 </footer>
   )
